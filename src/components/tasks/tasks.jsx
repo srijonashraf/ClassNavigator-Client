808da93e96ecb25e578a926344af18329af64b96
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2024-01-31 12:43:36 +0600

    task sorted based on date

--- src/components/tasks/tasks.jsx
+++ src/components/tasks/tasks.jsx
@@ -121,70 +121,73 @@ const Tasks = ({ TaskApiRefresh }) => {
 
     const renderCourseCards = () => {
         return (
-            tasks && tasks.sort((a, b) => a._id.localeCompare(b._id)).map((task, index) => (
-                <div key={task._id} className="col-md-6 mb-4">
-                    <div className="card shadow-sm border border-light-subtle">
-                        <div className="card-body">
-                            {renderCountdown(task)}
-                            <Avatar name={task.type} className='bg-warning card-img-top w-100 rounded-top-2 mb-3' />
-                            <div className='mb-2'>
-                                <button className='btn btn-primary btn-sm rounded-1 float-end'>{task.type}</button>
-                                <button className='btn btn-secondary btn-sm rounded-1'>Group: {task.group}</button>
-                            </div>
-                            <p className="card-title fw-bold fs-5 title-color">{task.taskTitle}</p>
-                            <div className="card-subtitle mb-2 text-muted small">
-                                <span><u>Description:</u></span>
-                                <div>
-                                    {showFullDescription ? (
-                                        <>
-                                            <div dangerouslySetInnerHTML={{ __html: task.taskDescription }}></div>
-                                            <span onClick={toggleDescription} className="text-secondary cursorPointer">See Less</span>
-                                        </>
-                                    ) : (
-                                        <div className='d-flex gap-1'>
-                                            <div className='m-less' dangerouslySetInnerHTML={{ __html: task.taskDescription.substring(0, 100) }}></div>
-                                            {task.taskDescription.length > 100 && (
-                                                <span onClick={toggleDescription} className="text-primary cursorPointer">See More...</span>
-                                            )}
-                                        </div>
-                                    )}
+            tasks &&
+            tasks
+                .sort((a, b) => new Date(a.date) - new Date(b.date))
+                .map((task, index) => (
+                    <div key={task._id} className="col-md-6 mb-4">
+                        <div className="card shadow-sm border border-light-subtle">
+                            <div className="card-body">
+                                {renderCountdown(task)}
+                                <Avatar name={task.type} className='bg-warning card-img-top w-100 rounded-top-2 mb-3' />
+                                <div className='mb-2'>
+                                    <button className='btn btn-primary btn-sm rounded-1 float-end'>{task.type}</button>
+                                    <button className='btn btn-secondary btn-sm rounded-1'>Group: {task.group}</button>
                                 </div>
+                                <p className="card-title fw-bold fs-5 title-color">{task.taskTitle}</p>
+                                <div className="card-subtitle mb-2 text-muted small">
+                                    <span><u>Description:</u></span>
+                                    <div>
+                                        {showFullDescription ? (
+                                            <>
+                                                <div dangerouslySetInnerHTML={{ __html: task.taskDescription }}></div>
+                                                <span onClick={toggleDescription} className="text-secondary cursorPointer">See Less</span>
+                                            </>
+                                        ) : (
+                                            <div className='d-flex gap-1'>
+                                                <div className='m-less' dangerouslySetInnerHTML={{ __html: task.taskDescription.substring(0, 100) }}></div>
+                                                {task.taskDescription.length > 100 && (
+                                                    <span onClick={toggleDescription} className="text-primary cursorPointer">See More...</span>
+                                                )}
+                                            </div>
+                                        )}
+                                    </div>
+                                </div>
+
+
+                                <p className={`btn badge rounded-1 float-end ${task.mode === 'Online' ? 'btn-info' : 'btn-secondary'}`}>• {task.mode}</p>
+                                <div className='d-flex flex-row gap-2'>
+                                    <p className='fw-bold d-flex align-items-center gap-1'>
+                                        <LuCalendarCheck className='text-primary fs-5' /> {formatDate(new Date(task.date))}
+                                    </p>
+                                    <p className='fw-bold d-flex align-items-center gap-1'>
+                                        <IoTimeSharp className='text-primary fs-5' /> {formatTime(task.time)}
+                                    </p >
+                                </div>
+
+
+                                <div className="d-flex align-items-center gap-2">
+                                    {adminAccess(classId) &&
+                                        <MdDeleteOutline onClick={() => handleDeleteTask(classId, courseId, task._id)} className='fs-4 text-danger cursorPointer' />
+
+                                    }
+                                    {adminAccess(classId) &&
+                                        <Link to={`/tasks/${classId}/${courseId}/edit/${task._id}`}>
+                                            <div><FiEdit onClick={handleShowAddNewTask} className='fs-5 text-primary cursorPointer' /></div>
+                                        </Link>
+                                    }
+                                </div>
+                                <footer className='sm-text float-end mt-3 text-muted'>Edited: {new Date(task.updatedAt).toLocaleString("en-AU")}</footer>
                             </div>
-
-
-                            <p className={`btn badge rounded-1 float-end ${task.mode === 'Online' ? 'btn-info' : 'btn-secondary'}`}>• {task.mode}</p>
-                            <div className='d-flex flex-row gap-2'>
-                                <p className='fw-bold d-flex align-items-center gap-1'>
-                                    <LuCalendarCheck className='text-primary fs-5' /> {formatDate(new Date(task.date))}
-                                </p>
-                                <p className='fw-bold d-flex align-items-center gap-1'>
-                                    <IoTimeSharp className='text-primary fs-5' /> {formatTime(task.time)}
-                                </p >
-                            </div>
-
-
-                            <div className="d-flex align-items-center gap-2">
-                                {adminAccess(classId) &&
-                                    <MdDeleteOutline onClick={() => handleDeleteTask(classId, courseId, task._id)} className='fs-4 text-danger cursorPointer' />
-
-                                }
-                                {adminAccess(classId) &&
-                                    <Link to={`/tasks/${classId}/${courseId}/edit/${task._id}`}>
-                                        <div><FiEdit onClick={handleShowAddNewTask} className='fs-5 text-primary cursorPointer' /></div>
-                                    </Link>
-                                }
-                            </div>
-                            <footer className='sm-text float-end mt-3 text-muted'>Edited: {new Date(task.updatedAt).toLocaleString("en-AU")}</footer>
                         </div>
                     </div>
-                </div>
-            ))
+                ))
         );
     };
 
     return (
         <div className="row">
-          <Breadcrumb>
+            <Breadcrumb>
                 <Breadcrumb.Item onClick={() => navigate('/')} >Home</Breadcrumb.Item>
                 <Breadcrumb.Item onClick={() => navigate(`/courses/${classId}`)}>
                     Courses
