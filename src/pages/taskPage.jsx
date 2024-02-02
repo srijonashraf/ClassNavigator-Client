53c801195f19cc3af13bfe61f24ed272bf521e4a
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2024-02-03 01:16:38 +0600

    task filter removed from this page

--- src/pages/taskPage.jsx
+++ src/pages/taskPage.jsx
@@ -2,17 +2,17 @@ import React, { useEffect, useState } from 'react';
 import AppNavbar from './../components/shared/AppNavbar';
 import ProfileStore from '../stores/ProfileStore';
 import ContentStore from '../stores/ContentStore';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import Tasks from '../components/tasks/tasks';
+import { IoMdArrowDropdown } from "react-icons/io";
 
 const TaskPage = () => {
     const [change, setChange] = useState(0);
     const { ProfileDetailsRequest } = ProfileStore();
-    const { FetchAllTasksByCourseRequest } = ContentStore();
+    const { FetchAllTasksByCourseRequest, FetchCompletedTaskByCourseRequest } = ContentStore();
 
     const { classId } = useParams();
     const { courseId } = useParams();
-
 
     useEffect(() => {
         const fetchData = async () => {
