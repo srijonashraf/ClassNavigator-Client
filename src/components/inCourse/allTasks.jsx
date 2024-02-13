3efa41d1b39fd53df7fdd85ef6e56a3e7069af89
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2024-02-13 13:05:33 +0600

    design lining issue fixed

--- src/components/inCourse/allTasks.jsx
+++ src/components/inCourse/allTasks.jsx
@@ -1,12 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { MdDeleteOutline } from "react-icons/md";
-import { FiEdit } from "react-icons/fi"
-import { Link, useParams, useNavigate } from 'react-router-dom';
-import LoadingBarComponent from './../loading/loadingBar';
-import { DeleteTask } from '../../apirequest/apiRequest';
-import { errorToast, successToast } from "../../helper/ToasterHelper.js";
+import { Link } from 'react-router-dom';
 import ContentStore from '../../stores/ContentStore.js';
-import ProfileStore from '../../stores/ProfileStore.js';
 import Avatar from 'react-avatar';
 import { LuCalendarCheck } from "react-icons/lu";
 import { IoTimeSharp } from "react-icons/io5";
@@ -14,17 +8,10 @@ import { IoTimeSharp } from "react-icons/io5";
 const AllTasks = () => {
 
     const [isLoading, setIsLoading] = useState(true);
-    const [showAddNewTask, setShowAddNewTask] = useState(false);
-    const [progress, setProgress] = useState(0);
     const [showFullDescription, setShowFullDescription] = useState(false);
-    const [change, setChange] = useState('');
 
     const { FetchAllTaskCommonStore, FetchAllTasksByCourseRequest, FetchAllCourses, FetchAllTasksRequest } = ContentStore();
-    const { AdminAccessClasses } = ProfileStore();
 
-
-
-    const adminAccess = (classId) => AdminAccessClasses && AdminAccessClasses.includes(classId);
 
     const toggleDescription = () => {
         setShowFullDescription(!showFullDescription);
@@ -59,16 +46,15 @@ const AllTasks = () => {
         return (
             <div>
                 {remainingTime > 0 ? (
-                    <div className='d-flex justify-content-between'>
-                        <div>
-                            <span className='fw-bold'>Time Left: </span>
-                            {days > 0 && <span className='fw-bold'>{days}d </span>}
-                            <span className={`fw-bold ${days > 0 || hours > 1 ? 'text-dark' : 'text-danger'}`}>
-                                {hours}h {minutes}m
-                            </span>
-                        </div>
 
+                    <div>
+                        <span className='fw-bold'>Time Left: </span>
+                        {days > 0 && <span className='fw-bold'>{days}d </span>}
+                        <span className={`fw-bold ${days > 0 || hours > 1 ? 'text-dark' : 'text-danger'}`}>
+                            {hours}h {minutes}m
+                        </span>
                     </div>
+
                 ) : null}
             </div>
 
@@ -116,8 +102,11 @@ const AllTasks = () => {
                             <div key={task._id} className="col-md-6 mb-4 mt-5">
                                 <div className="card shadow-sm border border-light-subtle">
                                     <div className={`card-body ${new Date(task.date + ' ' + task.time) < new Date() ? 'bg-expired' : ''}`}>
-                                        {renderCountdown(task)}
-                                        <p className='btn btn-danger badge float-end'>{task?.status}</p>
+
+                                        <div className='d-flex justify-content-between mb-2'>
+                                            {renderCountdown(task)}
+                                            <p className='btn btn-danger badge float-end'>{task?.status}</p>
+                                        </div>
                                         <Avatar name={task.type} className='bg-warning card-img-top w-100 rounded-top-2 mb-3' />
                                         <div className='mb-2'>
                                             <button className='btn btn-primary btn-sm rounded-1 float-end'>{task.type}</button>
@@ -155,9 +144,9 @@ const AllTasks = () => {
 
 
                                         <div className="d-flex align-items-center gap-2">
-                                                <Link to={`/tasks/${task?.classId}/${task?.courseId}?taskId=${task?._id}`}>
-                                                    <div><button className='btn btn-dark rounded-1 cursorPointer'>Go to Task</button></div>
-                                                </Link>
+                                            <Link to={`/tasks/${task?.classId}/${task?.courseId}?taskId=${task?._id}`}>
+                                                <div><button className='btn btn-dark rounded-1 cursorPointer my-2'>Go to Task</button></div>
+                                            </Link>
                                         </div>
                                         <footer className='sm-text float-end mt-3 text-muted'>Edited: {new Date(task.updatedAt).toLocaleString("en-AU")}</footer>
                                     </div>
