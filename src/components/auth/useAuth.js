2b66a27d5de0be726f7131bcfb3d2f8fdbecdc4b
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2024-02-16 12:39:02 +0600

    modified

--- src/components/auth/useAuth.js
+++ src/components/auth/useAuth.js
@@ -1,6 +1,6 @@
 import Cookies from "js-cookie";
 //Checking if user is logged in or not
 export default function useAuth() {
-  const auth = Cookies.get("accessToken");
+  const auth = Cookies?.get("accessToken");
   return auth ? true : false;
 }
