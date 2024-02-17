a7c50ddc80a0abf667d43ae3ed3e89567ab727e0
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2024-02-17 23:31:12 +0600

    newAdmin add option added

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import CoursePage from './pages/coursePage';
 import useAuth from './components/auth/useAuth.js';
 import TaskPage from './pages/taskPage';
 import InCoursePage from './pages/inCoursePage';
+import NewAdminPage from './pages/newAdminPage';
 
 
 const App = () => {
@@ -28,17 +29,18 @@ const App = () => {
                             <Route path="/register" element={<RegistrationPage />} />
                         </>
                     )}
-                    
+
                     <Route path="/" element={<PrivateRoute><DashboardPage /></PrivateRoute>} />
                     <Route path="/dashboard" element={<PrivateRoute><DashboardPage /></PrivateRoute>} />
                     <Route path="/classes/edit/:classId" element={<PrivateRoute><DashboardPage /></PrivateRoute>} />
+                    <Route path="/classes/newAdmin/:classId" element={<PrivateRoute><NewAdminPage /></PrivateRoute>} />
                     <Route path="/courses/:classId" element={<PrivateRoute><CoursePage /></PrivateRoute>} />
                     <Route path="/courses/:classId/edit/:courseId" element={<PrivateRoute><CoursePage /></PrivateRoute>} />
                     <Route path="/tasks/:classId/:courseId" element={<PrivateRoute><TaskPage /></PrivateRoute>} />
                     <Route path="/tasks/:classId/:courseId/edit/:taskId" element={<PrivateRoute><TaskPage /></PrivateRoute>} />
                     <Route path="/inCourse/allTasks" element={<PrivateRoute><InCoursePage /></PrivateRoute>} />
 
-                    <Route path="/*" element={<Fragment><div className='d-flex flex-column vh-100 align-items-center justify-content-center' ><p className='text-center fs-2'>Page Not Found</p><button className="btn btn-primary" onClick={() => window.location.href = "/dashboard"}>Return to Home</button></div></Fragment>} />
+                    <Route path="/*" element={<Fragment><div className='d-flex flex-column vh-100 align-items-center justify-content-center' ><p className='text-center fs-2'>Page Not Found</p><button className="btn btn-primary my-2" onClick={() => window.location.href = "/dashboard"}>Return to Home</button></div></Fragment>} />
                 </Routes>
             </Router>
         </Fragment>
