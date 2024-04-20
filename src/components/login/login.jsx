3efe9f2abce135fa3932abd41e83b3bf5232b352
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2024-04-20 08:09:12 +0600

    updated footer

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,68 +1,88 @@
-import React from 'react';
+import React from "react";
 import { Login as LoginApi } from "../../api/apiRequest.js";
 import { errorToast, successToast } from "../../helper/ToasterHelper.js";
-import { useNavigate } from 'react-router-dom';
-import logo from '../../assets/img/favicon.png';
-
+import { NavLink, useNavigate } from "react-router-dom";
+import logo from "../../assets/img/favicon.png";
 
 const Login = () => {
+  const [formValue, setFormValue] = React.useState({
+    userId: "",
+    password: "",
+  });
 
-    const [formValue, setFormValue] = React.useState({
-        userId: "",
-        password: ""
-    });
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
-
-    const loginRequest = async (e) => {
-        e.preventDefault();
-        if (formValue.userId.length === 0 || formValue.password.length === 0) {
-            errorToast("Please enter all the fields");
-        } else {
-            const response = await LoginApi(formValue);
-            if (response) {
-                successToast('Login Successful');
-                navigate('/dashboard');
-            } else {
-                errorToast('Wrong Credentials!');
-            }
-        }
+  const loginRequest = async (e) => {
+    e.preventDefault();
+    if (formValue.userId.length === 0 || formValue.password.length === 0) {
+      errorToast("Please enter all the fields");
+    } else {
+      const response = await LoginApi(formValue);
+      if (response) {
+        successToast("Login Successful");
+        navigate("/dashboard");
+      } else {
+        errorToast("Wrong Credentials!");
+      }
     }
+  };
 
-    return (
-        <div className="row justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
-            <div className="col-lg-6 col-md-8 col-sm-10">
-                <div className="card border-0 shadow-lg rounded-1 p-4">
-                    <div className="card-body">
-                        <form onSubmit={loginRequest} className="form d-flex flex-column gap-3">
-
-                            <div className='d-flex flex-column gap-2 align-items-center'>
-                                <img src={logo} className='img-fluid w-25 mx-auto d-block' alt="" />
-                                <h3 className='text-center'>ClassNavigator</h3>
-                            </div>
-                            <input
-                                type="text"
-                                placeholder="Student ID"
-                                value={formValue.userId}
-                                onChange={(e) => setFormValue({ ...formValue, userId: e.target.value })}
-                                className="form-control"
-                            />
-                            <input
-                                type="password"
-                                placeholder="Password"
-                                value={formValue.password}
-                                onChange={(e) => setFormValue({ ...formValue, password: e.target.value })}
-                                className="form-control"
-                            />
-                            <button type="submit" className="btn btn-primary">
-                                Login
-                            </button>
-                        </form>
-                    </div>
-                </div>
+  return (
+    <div
+      className="row justify-content-center align-items-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="col-lg-6 col-md-8 col-sm-10">
+        <div className="card border-0 shadow-lg rounded-1 p-4">
+          <div className="card-body">
+            <form
+              onSubmit={loginRequest}
+              className="form d-flex flex-column gap-3"
+            >
+              <div className="d-flex flex-column gap-2 align-items-center">
+                <img
+                  src={logo}
+                  className="img-fluid w-25 mx-auto d-block"
+                  alt=""
+                />
+                <h3 className="text-center">ClassNavigator</h3>
+              </div>
+              <input
+                type="text"
+                placeholder="Student ID"
+                value={formValue.userId}
+                onChange={(e) =>
+                  setFormValue({ ...formValue, userId: e.target.value })
+                }
+                className="form-control"
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                value={formValue.password}
+                onChange={(e) =>
+                  setFormValue({ ...formValue, password: e.target.value })
+                }
+                className="form-control"
+              />
+              <button type="submit" className="btn btn-primary">
+                Login
+              </button>
+            </form>
+            <div className="footer d-flex gap-2 float-end mt-3 fw-bold">
+              <p>
+                <NavLink to={"/register"} className="nav-link">
+                  Sign Up
+                </NavLink>
+              </p>
+              <p>|</p>
+              <p>Forget Password</p>
             </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/components/registration/registration.jsx
+++ src/components/registration/registration.jsx
@@ -1,108 +1,133 @@
-import React from 'react';
-import { Register } from '../../api/apiRequest.js';
+import React from "react";
+import { Register } from "../../api/apiRequest.js";
 import { errorToast, successToast } from "../../helper/ToasterHelper.js";
-import { UserIdValidation } from '../../helper/FormHelper.js';
+import { UserIdValidation } from "../../helper/FormHelper.js";
+import { NavLink, useNavigate } from "react-router-dom";
 
 const Registration = () => {
+  const [formValue, setFormValue] = React.useState({
+    userId: "",
+    name: "",
+    email: "",
+    password: "",
+    section: "",
+  });
 
-    const [formValue, setFormValue] = React.useState({
-        userId: "",
-        name: "",
-        email: "",
-        password: "",
-        section: "",
-    });
+  const navigate = useNavigate();
 
-    const registrationRequest = async (e) => {
-        e.preventDefault();
+  const registrationRequest = async (e) => {
+    e.preventDefault();
 
-        try {
-            if (
-                formValue.email.length === 0 ||
-                formValue.password.length === 0 ||
-                formValue.userId.length === 0 ||
-                formValue.name.length === 0 ||
-                formValue.section.length === 0
-            ) {
-                errorToast("Please enter all the fields");
-            }
+    try {
+      if (
+        formValue.email.length === 0 ||
+        formValue.password.length === 0 ||
+        formValue.userId.length === 0 ||
+        formValue.name.length === 0 ||
+        formValue.section.length === 0
+      ) {
+        errorToast("Please enter all the fields");
+      } else if (UserIdValidation(formValue.userId) === false) {
+        errorToast("Invalid Student Id");
+      } else {
+        const response = await Register(formValue);
+        if (response) {
+          successToast("Registration Successful");
+          navigate("/login");
 
-
-            else if (UserIdValidation(formValue.userId) === false) {
-                errorToast("Invalid Student Id");
-            } else {
-                const response = await Register(formValue);
-                if (response) {
-                    successToast('Registration Successful');
-                    setFormValue({
-                        userId: "",
-                        name: "",
-                        email: "",
-                        password: "",
-                        section: "",
-                    });
-
-                } else {
-                    errorToast('User Already Exists');
-                }
-            }
-        } catch (error) {
-            console.error(error);
-            errorToast('Internal Server Error');
-        } finally {
-            console.log('Registration request completed');
-
+          setFormValue({
+            userId: "",
+            name: "",
+            email: "",
+            password: "",
+            section: "",
+          });
+        } else {
+          errorToast("User Already Exists");
         }
-    };
+      }
+    } catch (error) {
+      console.error(error);
+      errorToast("Internal Server Error");
+    } finally {
+      console.log("Registration request completed");
+    }
+  };
 
-    return (
-        <div className='row justify-content-center align-items-center' style={{ minHeight: '100vh' }}>
-            <div className="col-lg-6 col-md-8 col-sm-10">
-                <div className="card border-primary shadow-lg rounded-1 p-4">
-                    <div className="card-body">
-                        <form onSubmit={registrationRequest} className="form d-flex flex-column gap-3">
-                            <input
-                                type="text"
-                                placeholder="Student ID, eg. 201-15-12345, 23245795221479"
-                                value={formValue.userId}
-                                onChange={(e) => setFormValue({ ...formValue, userId: e.target.value })}
-                                className="form-control"
-                            />
-                            <input
-                                type="text"
-                                placeholder="Name"
-                                value={formValue.name}
-                                onChange={(e) => setFormValue({ ...formValue, name: e.target.value })}
-                                className="form-control"
-                            />
-                            <input
-                                type="email"
-                                placeholder="Email"
-                                value={formValue.email}
-                                onChange={(e) => setFormValue({ ...formValue, email: e.target.value })}
-                                className="form-control"
-                            />
-                            <input
-                                type="password"
-                                placeholder="Password"
-                                value={formValue.password}
-                                onChange={(e) => setFormValue({ ...formValue, password: e.target.value })}
-                                className="form-control"
-                            />
-                            <input
-                                type="text"
-                                placeholder="Section"
-                                value={formValue.section}
-                                onChange={(e) => setFormValue({ ...formValue, section: e.target.value })}
-                                className="form-control"
-                            />
-                            <button type="submit" className="btn btn-primary">Register</button>
-                        </form>
-                    </div>
-                </div>
+  return (
+    <div
+      className="row justify-content-center align-items-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="col-lg-6 col-md-8 col-sm-10">
+        <div className="card border-primary shadow-lg rounded-1 p-4">
+          <div className="card-body">
+            <form
+              onSubmit={registrationRequest}
+              className="form d-flex flex-column gap-3"
+            >
+              <input
+                type="text"
+                placeholder="Student ID, eg. 201-15-12345, 23245795221479"
+                value={formValue.userId}
+                onChange={(e) =>
+                  setFormValue({ ...formValue, userId: e.target.value })
+                }
+                className="form-control"
+              />
+              <input
+                type="text"
+                placeholder="Name"
+                value={formValue.name}
+                onChange={(e) =>
+                  setFormValue({ ...formValue, name: e.target.value })
+                }
+                className="form-control"
+              />
+              <input
+                type="email"
+                placeholder="Email"
+                value={formValue.email}
+                onChange={(e) =>
+                  setFormValue({ ...formValue, email: e.target.value })
+                }
+                className="form-control"
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                value={formValue.password}
+                onChange={(e) =>
+                  setFormValue({ ...formValue, password: e.target.value })
+                }
+                className="form-control"
+              />
+              <input
+                type="text"
+                placeholder="Section"
+                value={formValue.section}
+                onChange={(e) =>
+                  setFormValue({ ...formValue, section: e.target.value })
+                }
+                className="form-control"
+              />
+              <button type="submit" className="btn btn-primary">
+                Register
+              </button>
+            </form>
+            <div className="footer float-end mt-3">
+              <p className="d-flex gap-2">
+                Already Have an account?
+                <span>
+                  <NavLink to={"/login"}>Login</NavLink>
+                </span>
+              </p>
             </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Registration;
